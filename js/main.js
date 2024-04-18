4a0ac5a22174de3ea67f3cdb8fa9d501d4ace57b
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-18 18:01:57 +0200

    fixed bug event listener

--- js/main.js
+++ js/main.js
@@ -9,19 +9,24 @@ const ageUser = document.getElementById("age");
 
 const date = document.getElementById("send-date");
 date.addEventListener ("click", function() {
-    console.log("quanti km fa?", kmToTravel.value)
-    console.log("quanti anni ha?", ageUser.value)
-}) 
-
-const priceTicket = kmRate * kmToTravel.value
-console.log(`Prezzo base del biglietto: ${priceTicket}`);
+    console.log("quanti km fa?", kmToTravel.value);
+    console.log("quanti anni ha?", ageUser.value);
+    
+    const priceTicket = kmRate * kmToTravel.value
+    priceTicketToFixed = priceTicket.toFixed();
+    console.log(`Prezzo base del biglietto: ${priceTicketToFixed}`);
 
 //Se l'utente è minorenne
 if (ageUser.value <18) {
     let priceJunior = (priceTicket * 0.8);
-    console.log(`Prezzo Ticket con sconto per minorenni: ${priceJunior}`);
+    priceJuniorToFixed = priceJunior.toFixed(2);
+    console.log(`Prezzo Ticket con sconto per minorenni: ${priceJuniorToFixed}`);
 }
 else if (ageUser.value >65) {
     let priceSenior = (priceTicket * 0.6);
-    console.log(`Prezzo Ticket con sconto per senior: ${priceSenior}`);
+    priceSeniorToFixed = priceSenior.toFixed(2);
+    console.log(`Prezzo Ticket con sconto per senior: ${priceSeniorToFixed}`);
 }
+}) 
+
+
