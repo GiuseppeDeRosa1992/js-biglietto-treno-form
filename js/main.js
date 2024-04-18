836947f26b28d21532f3bb51ad058766c7292cd4
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-04-18 18:58:45 +0200

    added print inside html

--- js/main.js
+++ js/main.js
@@ -2,6 +2,11 @@
 const kmRate = 0.21;
 console.log(`Tariffa fissa per km: ${kmRate}`);
 
+// Stringa
+string = "Il prezzo base è:"
+stringJunior = "Il prezzo scontato per i minorenni è:"
+stringSenior = "Il prezzo scontato per i senior è:"
+
 // CHIEDO ALL'UTENTE KM DA PERCORRERE E ETA' TRAMITE INPUT
 const kmToTravel = document.getElementById("km");
 
@@ -21,12 +26,19 @@ if (ageUser.value <18) {
     let priceJunior = (priceTicket * 0.8);
     priceJuniorToFixed = priceJunior.toFixed(2);
     console.log(`Prezzo Ticket con sconto per minorenni: ${priceJuniorToFixed}`);
+    document.getElementById("result-junior").innerHTML = stringJunior + " " + priceJuniorToFixed;
 }
+//Se l'utente è senior
 else if (ageUser.value >65) {
     let priceSenior = (priceTicket * 0.6);
     priceSeniorToFixed = priceSenior.toFixed(2);
     console.log(`Prezzo Ticket con sconto per senior: ${priceSeniorToFixed}`);
+    document.getElementById("result-senior").innerHTML = stringSenior + " " + priceSeniorToFixed;
 }
+
+document.getElementById("result").innerHTML = string + " " + priceTicketToFixed
 }) 
+
+
 
 
