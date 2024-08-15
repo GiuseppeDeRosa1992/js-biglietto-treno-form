3c220971ed58c95c327cb41807466bd2a418d771
GiuseppeDeRosa1992 <derosagiuseppe1092@gmail.com>
2024-08-15 17:03:15 +0200

    added validation html required, min and max, and added in js nameArray for generate random name and change event listener from click to submit

--- js/main.js
+++ js/main.js
@@ -12,37 +12,65 @@ const kmToTravel = document.getElementById("km");
 
 const ageUser = document.getElementById("age");
 
-const date = document.getElementById("send-date");
-date.addEventListener ("click", function() {
+
+const nameArray = [
+    "De Rosa Giuseppe",
+    "Damiani Eleonora",
+    "De Rosa Antonio Ciro",
+    "Russo Teresa",
+    "Cori Davide",
+    "Magnasciutto Riccardo",
+    "Cognigni Lorenzo",
+    "Eleonora Pimpolari",
+    "Eleonora Ferlicca",
+    "Zannini Federico",
+    "Conticchio Chiara",
+    "Arbola Davide",
+    "Falcone Francesco",
+    "Federici Matteo",
+];
+
+let nameFinal = nameArray[Math.floor(Math.random() * nameArray.length)];
+console.log(nameFinal)
+
+//CREO VARIABILE DATE DOVE RICHIAMO TRAMITE QUERY SELECTOR IL FORM
+const date = document.querySelector("form");
+//EVENT LISTENER SUBMIT CHE QUANDO PREMO SUL BOTTONE MI FA VEDERE I DATI
+date.addEventListener("submit", function (eventIntercetted) {
+
+    //Intercetto l'evento e quando clicco sul bottone la pagina non si refresha da sola e i dati mi rimangono in pagina
+    eventIntercetted.preventDefault()
+
     console.log("quanti km fa?", kmToTravel.value);
     console.log("quanti anni ha?", ageUser.value);
-    
+
     const priceTicket = kmRate * kmToTravel.value
     priceTicketToFixed = priceTicket.toFixed();
     console.log(`Prezzo base del biglietto: ${priceTicketToFixed}`);
 
-//Se l'utente è minorenne
-if (ageUser.value <18) {
-    let priceJunior = (priceTicket * 0.8);
-    priceJuniorToFixed = priceJunior.toFixed(2);
-    console.log(`Prezzo Ticket con sconto per minorenni: ${priceJuniorToFixed}`);
-    document.getElementById("result-junior").innerHTML = stringJunior + " " + priceJuniorToFixed;
-}
-//Se l'utente è senior
-else if (ageUser.value >65) {
-    let priceSenior = (priceTicket * 0.6);
-    priceSeniorToFixed = priceSenior.toFixed(2);
-    console.log(`Prezzo Ticket con sconto per senior: ${priceSeniorToFixed}`);
-    document.getElementById("result-senior").innerHTML = stringSenior + " " + priceSeniorToFixed;
-}
+    //Se l'utente è minorenne
+    if (ageUser.value < 18) {
+        let priceJunior = (priceTicket * 0.8);
+        priceJuniorToFixed = priceJunior.toFixed(2);
+        console.log(`Prezzo Ticket con sconto per minorenni: ${priceJuniorToFixed}`);
+        document.getElementById("result-junior").innerHTML = stringJunior + " " + priceJuniorToFixed + " " + "€";
+    }
+    //Se l'utente è senior
+    else if (ageUser.value > 65) {
+        let priceSenior = (priceTicket * 0.6);
+        priceSeniorToFixed = priceSenior.toFixed(2);
+        console.log(`Prezzo Ticket con sconto per senior: ${priceSeniorToFixed}`);
+        document.getElementById("result-senior").innerHTML = stringSenior + " " + priceSeniorToFixed + " " + "€";
+    }
 
-const numberPlace = ageUser.value * 1.2;
-numberPlaceToFixed = numberPlace.toFixed()
+    const numberPlace = ageUser.value * 1.2;
+    numberPlaceToFixed = numberPlace.toFixed()
 
-document.getElementById("result").innerHTML = string + " " + priceTicketToFixed
-document.getElementById("number-place").innerHTML = `"Il suo posto è il n. ${numberPlaceToFixed}"`;
-document.getElementById("train-carriage").innerHTML = "La sua carrozza è la n. 13"
-}) 
+    document.getElementById("name-user").innerHTML = nameFinal;
+    document.getElementById("result").innerHTML = string + " " + priceTicketToFixed + " " + "€";
+    document.getElementById("number-place").innerHTML = `"Il suo posto è il n. ${numberPlaceToFixed}"`;
+    document.getElementById("train-carriage").innerHTML = "La sua carrozza è la n. 13"
+})
 
 
 
